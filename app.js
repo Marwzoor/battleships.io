34705c7dd0d54e3c0aa765c33710319fe1e838fd
Martin Werner <carlmartinwerner@gmail.com>
2018-02-19 21:28:05 +0100

    Added more stuff

--- app.js
+++ app.js
@@ -5,8 +5,28 @@ var io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
+var users = {};
+
 io.on('connection', function (socket) {
-	
+	socket.emit('init', {users: users});
+
+	socket.on('disconnect', function (data) {
+		socket.broadcast.emit('disconnected', {id: this.id});
+		delete users[socket.id];
+	});
+
+	socket.on('move', function (data) {
+		socket.broadcast.emit('move', {data: data, player: this.id});
+		users[this.id].x = data.x;
+		users[this.id].y = data.y;
+	});
+
+	users[socket.id] = {
+		x: 0,
+		y: 0
+	};
+
+	socket.broadcast.emit('connected', {id: socket.id});
 });
 
 http.listen(8080, function() {
--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -2,17 +2,44 @@ function BattleShip(locX, locY, rotate) {
 
 	this.locX = locX;
 	this.locY = locY;
-	this.speed = 0.01;
-	this.rotate = rotate;
+	this.speed = 5;
+	this.rotate = rotate * (Math.PI / 180);
 
 	this.update = function() {
-		var moveX = Math.cos(rotate);
-		var moveY = Math.sin(rotate);
+		var moveX = Math.cos(this.rotate);
+		var moveY = Math.sin(this.rotate);
 
 		this.locX += this.speed * moveX;
 		this.locY += this.speed * moveY;
 	};
 
+	this.draw = function(ctx) {
+		ctx.fillRect(this.locX-5, this.locY-5, 10, 10);
+	};
+
+	this.setRotation = function(rotate) {
+		this.rotate = rotate * (Math.PI / 180);
+	};
+
+	this.getLocX = function() {
+		return this.locX;
+	};
+
+	this.getLocY = function() {
+		return this.locY;
+	};
+
+	this.setLocX = function(locX) {
+		this.locX = locX;
+	};
+
+	this.setLocY = function(locY) {
+		this.locY = locY;
+	};
+
+	this.getRotation = function() {
+		return this.rotate;
+	};
 }
 
 window.onload = function() {
@@ -20,9 +47,6 @@ window.onload = function() {
 	var battleShipImg = new Image();
 
 	battleShipImg.onload = function() {
-		var currentBattleship;
-		var battleShips = [];
-
 		var gameCanvas = document.getElementById("game-area");
 		var gameContext = gameCanvas.getContext("2d");
 
@@ -36,8 +60,79 @@ window.onload = function() {
 			gameCanvas.height = window.innerHeight * pixelDensity;
 		};
 
+		var startX = Math.round(Math.random()*(gameCanvas.width-5)),
+			startY = Math.round(Math.random()*(gameCanvas.height-5)),
+			startRotation = Math.round(Math.random() * 360);
+
+		var currentBattleship = new BattleShip(startX, startY, startRotation);
+
+		var teamBattleShips = {};
+
 		var socket = io();
+
+		socket.on('init', function (data) {
+
+			for(var i in data.users) {
+				var user = data.users[i];
+
+				teamBattleShips[i] = new BattleShip(user.x, user.y, 0);
+			}
+
+			socket.on('connected', function (data) {
+				teamBattleShips[data.id] = new BattleShip(0, 0, 0);
+			});
+
+			socket.on('disconnected', function (data) {
+				delete teamBattleShips[data.id];
+			});
+
+			socket.on('move', function (data) {
+				var battleShip = teamBattleShips[data.player];
+
+				battleShip.setLocX(data.data.x);
+				battleShip.setLocY(data.data.y);
+			});
+
+			var update = function() {
+				currentBattleship.update();
+				socket.emit('move', {x: currentBattleship.getLocX(), y: currentBattleship.getLocY()});
+			};
+
+			var draw = function() {
+				gameContext.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+				currentBattleship.draw(gameContext);
+
+				for(var i in teamBattleShips) {
+					teamBattleShips[i].draw(gameContext);
+				}
+			};
+
+			var animate = function() {
+				update();
+				draw();
+
+				window.requestAnimationFrame(animate);
+			};
+
+			animate();
+
+			window.onmousemove = function (e) {
+				e = e || window.event;
+
+				var angle = Math.atan2(e.pageY - (currentBattleship.getLocY() / pixelDensity), e.pageX - (currentBattleship.getLocX() / pixelDensity));
+
+				angle *= 180 / Math.PI;
+
+				if(angle < 0)
+					angle += 360;
+
+				currentBattleship.setRotation(angle);
+			};
+		});
+		
 	};
 
 	battleShipImg.src = "/assets/images/ship.png";
+
 };
